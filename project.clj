562a685f70fae908d31a37de2096e888ca6047cd
Kimbsy <lordkimber@gmail.com>
2023-05-05 20:27:46 +0100

    Bump version

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject com.github.kimbsy/lein-template.quip "1.0.0"
-  :description "A Leiningen template for the Quip game library"
+(defproject com.github.kimbsy/lein-template.quip "1.0.1"
+  :description "A Leiningen template for the quip game engine"
   :url "https://github.com/Kimbsy/quip-template"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :eval-in-leiningen true
   :deploy-repositories [["clojars" {:url           "https://clojars.org/repo/"
                                     :username      :env/clojars_user
-                                    :password      :env/clojars_pass
+                         x           :password      :env/clojars_pass
                                     :sign-releases false}]])
