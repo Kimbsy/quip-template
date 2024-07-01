45051d62dcf9486f5a9cd26514f2fbafbda8a208
Kimbsy <lordkimber@gmail.com>
2024-07-01 11:48:39 +0100

    Use latest version

--- resources/leiningen/new/quip/project.clj
+++ resources/leiningen/new/quip/project.clj
@@ -1,6 +1,6 @@
 (defproject {{name}} "0.1.0"
   :dependencies [[org.clojure/clojure "1.11.3"]
-                 [quip "2.0.7"]]
+                 [quip "3.0.1"]]
   :main ^:skip-aot {{name}}.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
