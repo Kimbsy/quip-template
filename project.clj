268b7dd6fc404eee7d92445bda2909464a038195
Kimbsy <lordkimber@gmail.com>
2024-12-16 13:08:15 +0000

    Version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.kimbsy/lein-template.quip "2.0.1"
+(defproject com.kimbsy/lein-template.quip "2.0.2"
   :description "A Leiningen template for the quip game engine"
   :url "https://github.com/Kimbsy/quip-template"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
--- resources/leiningen/new/quip/project.clj
+++ resources/leiningen/new/quip/project.clj
@@ -1,6 +1,6 @@
 (defproject {{name}} "0.1.0"
   :dependencies [[org.clojure/clojure "1.12.0"]
-                 [com.kimbsy/quip "4.0.1"]]
+                 [com.kimbsy/quip "4.0.2"]]
   :main ^:skip-aot {{name}}.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
